181e13c7d82ced52f875b830817c51ce98bbd61e
oltapllana <oltapllana@gmail.com>
2025-01-02 15:40:00 +0100

    first commit

--- src/Quran.jsx
+++ src/Quran.jsx
@@ -5,26 +5,30 @@ const Quran = () => {
     const [quranData , setQuranData] = useState([])
     const [surahsCount , setSurahsCount] = useState(0)
     const [ayahsCount, setAyahsCount] = useState(0)
-        async function getVerse(){
-            
 
-            
+    useEffect(() => {
+      getVerse()
+  }, [])
+
+        async function getVerse(){
             const url = "https://api.alquran.cloud/v1/quran/en.asad";
             const response = await fetch(url)
             const data = await response.json()
+          
            if(ayahsCount >= data.data.surahs[surahsCount].ayahs.length - 2){
             setAyahsCount(0)
             setSurahsCount(prev => prev + 1)
            }
             setQuranData(data)
             setAyahsCount(prev => prev + 1)
-           console.log(data)
+        
+        }
+        function playAudio(){
+          const utterance = new SpeechSynthesisUtterance(quranData.data.surahs[surahsCount].ayahs[ayahsCount].text);
+          window.speechSynthesis.speak(utterance);
         }
 
-
-        useEffect(() => {
-            getVerse()
-        }, [])
+    
 
   return (
     <div>
@@ -33,9 +37,10 @@ const Quran = () => {
         {quranData.data ? <>
             <h1>Arabic Name: {quranData.data.surahs[surahsCount].englishName ? quranData.data.surahs[surahsCount].englishName : ""}</h1>
         <h2>English: {quranData.data.surahs[surahsCount].englishNameTranslation ? quranData.data.surahs[surahsCount].englishNameTranslation : ""}</h2>
-            <p>Verse: {quranData.data.surahs[surahsCount].ayahs[ayahsCount].text ? quranData.data.surahs[surahsCount].ayahs[ayahsCount].text : ""}</p>
+            <p>{quranData.data.surahs[surahsCount].ayahs[ayahsCount].text ? quranData.data.surahs[surahsCount].ayahs[ayahsCount].text : ""}</p>
                             </> :<></>}
         <button onClick={getVerse}>Next verse</button>
+        <button className="audioReciter" onClick={playAudio}>Play audio</button>
       </div>
     </div>
   )
